57f6536ff1bf3281da2c9b33ae14323d2d850c53
Drew Hendrickson <dhendrickson@gmail.com>
2014-09-08 14:56:55 +0930

    remove everything possible

--- exp/js/exp.js
+++ exp/js/exp.js
@@ -1,21 +1,2 @@
-/*global $, document, window, console, alert, start */
+/*global $ */
 
-// this function runs automatically when the page is loaded
-$(document).ready(function () {
-
-  // show an alert if user tries to navigate away from this page
-  window.onbeforeunload = function() {
-      return "Are you sure you want to leave this page?";
-  };
-
-  // load all scrips then begin the experiment
-  $.when(
-    $.getScript( "http://code.jquery.com/jquery-latest.min.js" ),
-
-    $.Deferred(function( deferred ){
-      $( deferred.resolve );
-    })
-  ).done(function(){
-    startExperiment();
-  });
-});
